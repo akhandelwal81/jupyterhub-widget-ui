9043c28b753ea6ab0ba97d3d6020c05b29942140
akhandelwal81 <78117743+akhandelwal81@users.noreply.github.com>
2021-09-19 16:06:52 +0100

    Create data_processing.py

---
+++ src/data_processing.py
@@ -0,0 +1,13 @@
+```
+On Click of the Button on the UI, the method will be called and it can do potentially a number of things
+
+You can decide to upload a zipped file and this function can first help with pre-processing by unzipping the file
+It can also validate the file name and the schema of the data within the file
+Once it has passed the validations , it can actually perform data analytics using pandas, arrows etc
+Finally, you can decide to write the file to the file system which could be Unix, Windows or Hadoop
+
+```
+
+
+define data_process():
+    
